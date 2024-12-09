d794655ec6e2974aa0c1a3c4bd2b4bd7e506a60f
meisterjustice <eziefulejustice@gmail.com>
2024-12-09 12:50:38 +0100

    setup project

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="md:bg-black md:w-full md:h-full md:flex md:justify-center">
+      <div className="bg-white md:w-[395px] 2xl:w-96 min-h-screen md:h-screen md:scroll-auto md:self-center md:rounded-md"></div>
+    </div>
+  );
+}
