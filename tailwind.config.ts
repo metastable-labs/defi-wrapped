1daae166a54e832a48e94be6b4652296c55c1ab3
Favour Nwajoko <chocorazzee@gmail.com>
2024-12-10 18:09:14 +0100

    update: extend Tailwind CSS configuration to include assets directory and add new color shades

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,12 @@
 import type { Config } from 'tailwindcss';
 
 export default {
-  content: ['./pages/**/*.{js,ts,jsx,tsx,mdx}', './components/**/*.{js,ts,jsx,tsx,mdx}', './app/**/*.{js,ts,jsx,tsx,mdx}'],
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './assets/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
   theme: {
     extend: {
       colors: {
@@ -17,6 +22,15 @@ export default {
         150: '#0F172A',
         200: '#475569',
         250: '#334155',
+        300: '#4691FE',
+        350: '#020617',
+        400: '#C1FF2F',
+        450: '#B14DFF',
+        500: '#FF335C',
+        550: '#EBFAFF',
+        600: '#DF1C41',
+        650: '#EEEBFF',
+        700: '#E3EEFF',
         foreground: 'var(--foreground)',
       },
     },
