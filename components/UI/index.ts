9c863a16ad68c33e6b22c5040d46166994d70dac
Favour Nwajoko <chocorazzee@gmail.com>
2024-12-11 09:42:32 +0100

    refactor: remove unused Step export from UI components

--- components/UI/index.ts
+++ components/UI/index.ts
@@ -1,5 +1,4 @@
 import DWButton from './button';
 import DWClickAnimation from './click-animation';
-import { Step } from './step-cascade';
 
-export { DWButton, DWClickAnimation, Step };
+export { DWButton, DWClickAnimation };
